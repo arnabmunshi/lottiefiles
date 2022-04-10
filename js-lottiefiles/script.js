13165d00f959faf8b6c2852ede95646c5a13b691
Arnab Munshi <arnab47stars@gmail.com>
2022-04-10 21:37:23 +0530

    first commit

---
+++ js-lottiefiles/script.js
@@ -0,0 +1,14 @@
+bodymovin.loadAnimation({
+  container: document.getElementById("world-health-day"),
+  path: 'js-lottiefiles/world-health-day.json'
+});
+
+bodymovin.loadAnimation({
+  container: document.getElementById("world-heritage-day"),
+  path: 'js-lottiefiles/world-heritage-day.json'
+});
+
+bodymovin.loadAnimation({
+  container: document.getElementById("world-earth-day"),
+  path: 'js-lottiefiles/world-earth-day.json'
+});
